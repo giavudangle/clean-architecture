362d96743a55320dbef1f4acfbfbd4ace128d141
Cọp🐯 <danglegiavu@gmail.com>
2022-08-10 15:44:45 +0700

    build code-base for clean-achitecture project

---
+++ domain/src/main/java/SerializerInterface.java
@@ -0,0 +1,5 @@
+public interface SerializerInterface {
+    <T> byte[] serialize(T message) throws SerializingException;
+
+    <T> T deserialize(byte[] payload, Class<T> clazz) throws SerializingException;
+}
---
+++ domain/src/main/java/exception/SerializingException.java
@@ -0,0 +1,8 @@
+package exception;
+
+public class SerializingException {
+    public SerializingException(@NonNull final String message, @NonNull final Throwable e) {
+        super(message, e);
+    }
+
+}
